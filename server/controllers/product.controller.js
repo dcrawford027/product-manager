c3b23a7acd39ba24b63e25ad24f36159dbd2df68
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-09-14 21:58:07 -0400

    added list and details

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,8 +1,21 @@
 const { Product } = require('../models/product.model');
+const { request } = require('express');
 
 module.exports = {
     createProduct: (req,res) => {
         Product.create(req.body)
+            .then(data => res.json(data))
+            .catch(err => res.json(err))
+    },
+
+    getAllProducts: (req,res) => {
+        Product.find()
+            .then(data => res.json(data))
+            .catch(err => res.json(err))
+    },
+
+    getOneProduct: (req,res) => {
+        Product.findOne({ _id: req.params.id })
             .then(data => res.json(data))
             .catch(err => res.json(err))
     }
